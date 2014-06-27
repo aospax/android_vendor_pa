4575f1e503e64ee3015a924b86776adbc63d7147
Rob Beane <robbeane@gmail.com>
2014-06-27 16:45:20 -0400

    Add hammerhead bootanimation from Android L preview
    Change-Id: Icf0cf90831ab505054256cd84b8dfaff56575443
    Signed-off-by: Rob Beane <robbeane@gmail.com>

--- configs/bootanimation.mk
+++ configs/bootanimation.mk
@@ -19,7 +19,7 @@ ifneq ($(filter pa_mako pa_maguro pa_toro pa_toroplus pa_grouper pa_tilapia,$(TA
 endif
 ifeq (pa_hammerhead,$(TARGET_PRODUCT))
     PRODUCT_COPY_FILES += \
-        vendor/pa/prebuilt/bootanimation/1920x1080.zip:system/media/bootanimation.zip
+        vendor/pa/prebuilt/bootanimation/1920x1080-AndroidL.zip:system/media/bootanimation.zip
 endif
 ifneq ($(filter pa_deb pa_flo,$(TARGET_PRODUCT)),)
     PRODUCT_COPY_FILES += \
